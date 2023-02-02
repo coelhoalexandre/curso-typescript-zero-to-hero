81b6f7e3212cd7fa2d8ec3fc476ad50860ef6dfd
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2023-02-02 19:01:11 -0300

    Aula 47

--- modulo-04/aula-47-default-parameters/defaultParameters.ts
+++ modulo-04/aula-47-default-parameters/defaultParameters.ts
@@ -1,35 +1,30 @@
-/**
- * arquivo: defaultParameters.ts
- * descrição: arquivo responsável por ensinar como usar o
- * 'Default Parameters' em funções no no Typescript
- * data: 29/03/2022
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- */
-
 export {};
 
 // ==> Exemplo 01 - Default Parameters
+
 function descontoCompra(preco: number, desconto = 0.08) {
-  return preco * (1 - desconto);
+  return preco * (1 - desconto)
 }
 
-console.log(descontoCompra(100)); //92
+console.log(descontoCompra(100));
 
 // ==> Exemplo 02
+
 function exibirMensagem(mensagem: string, saudar = 'Fala, pessoal!') {
   return saudar + ' ' + mensagem + '!';
-}
+};
 
-console.log(exibirMensagem('JavaScript Developers'));
+console.log(exibirMensagem('JavaScript Developers!'));
 
 // ==> Exemplo 03
+
 function exibirNome(nome: string, sobrenome = 'Lemos') {
   return nome + ' ' + sobrenome;
-}
+};
 
 const resultado_1 = exibirNome('Glaucia');
 const resultado_2 = exibirNome('Glaucia', undefined);
-// const resultado_3 = exibirNome('Glaucia', 'de Souza', 'Senhorita');
+// const resultado_3 = exibirNome('Glaucia', 'de Souza', 'Lemos');
 const resultado_4 = exibirNome('Glaucia', 'de Souza');
 
 console.log(resultado_1);
