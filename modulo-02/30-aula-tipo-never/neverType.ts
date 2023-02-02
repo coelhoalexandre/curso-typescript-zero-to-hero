ccc0031a49c9f0386dab3bcddfd1044eef3fac21
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2023-02-02 14:38:34 -0300

    Aula 30

--- modulo-02/30-aula-tipo-never/neverType.ts
+++ modulo-02/30-aula-tipo-never/neverType.ts
@@ -1,22 +1,17 @@
-/**
- * arquivo: neverType.ts
- * descrição: arquivo responsável por ensinar conceitos básicos sobre o tipo 'never'
- * data: 11/11/2021
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#never
- */
-
 // ==> Exemplo 01: Never - Throw Exception
 
+import { CompletionTriggerKind } from "typescript";
+
 function error(message: string): never {
-  throw new Error(message);
+  throw new Error(message)
 }
 
-console.log(error('Erro de Mensagem - 01'));
+console.log(error('Erro de mensagem - 01'));
 
 // ==> Exemplo 02: Never inferido automaticamente
-function rejectMessage() {
-  return error('Error de Mensagem - 02');
+
+function rejectMessage() { // Automaticamente ele interpreta que é um never
+  return error('Erro de Mensagem - 02');
 }
 
 console.log(rejectMessage());
@@ -24,20 +19,19 @@ console.log(rejectMessage());
 // ==> Exemplo 03 - Função que contém loop infinito retorna o tipo 'never'
 
 const loopInfinito = function loop() {
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
+  while(true) {
     console.log('Oi, Developers!');
   }
 };
 
-// console.log(loopInfinito());
+// console.log(loopInfinito()); // Vai travar a maquina
 
 // ==> Exemplo 04: Diferença entre os tipos: 'never' x 'void'
 
 const algumaCoisaVoid: void = null;
 
-// ==> dará erro! (// Error: Type 'null' is not assignable to type 'never')
-// const algumaCoisaNever: never = null;
+//const algumaCoisaNever: never = null;
 
-//console.log(algumaCoisaVoid);
-//console.log(algumaCoisaNever);
+console.log(algumaCoisaVoid);
+
+//console.log(algumaCoisaNever); // Vai travar a maquina
