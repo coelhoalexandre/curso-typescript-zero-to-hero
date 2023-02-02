7e37bfc04d40d9369a2a22c5c742889755e5b519
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2023-02-02 19:00:37 -0300

    aula 41

--- modulo-03/aula-41-loop-while/loop-while.ts
+++ modulo-03/aula-41-loop-while/loop-while.ts
@@ -1,11 +1,3 @@
-/* eslint-disable prettier/prettier */
-/**
- * arquivo: loop-while.ts
- * descrição: arquivo responsável por ensinar como usar o loop for no Typescript
- * data: 23/01/2021
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- */
-
 // ==> Exemplo 01 - while
 let contador = 0;
 
