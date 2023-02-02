fbd4258c98f638ae37363476b6bb8dd9c007ba19
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2023-02-02 19:01:23 -0300

    Aula 49

--- modulo-04/aula-49-rest-parameters/restParameters.ts
+++ modulo-04/aula-49-rest-parameters/restParameters.ts
@@ -1,15 +1,8 @@
-/**
- * arquivo: restParameters.ts
- * descrição: arquivo responsável por ensinar como usar o
- * 'Rest Parameters' em funções no no Typescript
- * data: 29/03/2022
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- */
-
 export {};
 
 // ==> Exemplo 01
-function somarNumeros(...numeros: number[]) {
+
+function somarNumeros(...numeros: number[]):number {
   let total = 0;
   numeros.forEach((numero) => (total += numero));
 
@@ -20,21 +13,20 @@ console.log(somarNumeros(30, 20));
 console.log(somarNumeros(40, 50, 50, 40, 20));
 
 // ==> Exemplo 02
-function listarFrutas(frase: string, ...frutas: string[]) {
+
+function listarFrutas(frase: string, ...frutas: string[]): string {
   return frase + ' ' + frutas.join(', ');
 }
 
-console.log(
-  listarFrutas(
-    'Glaucia, você precisa ir na feira para comprar...:',
-    '🥥',
-    '🍓',
-    '🍌',
-    '🍍',
-  ),
-);
+console.log(listarFrutas('Glaucia, você precisa ir na feira e comprar:',
+'🥥',
+'🍓',
+'🍌',
+'🍍',
+));
 
 // ==> Exemplo 03
+
 class Produtos {
   public exibirProdutos(...produtos: string[]): void {
     for (const produto of produtos) {
@@ -44,9 +36,7 @@ class Produtos {
 }
 
 const departamentoInformatica: Produtos = new Produtos();
-console.log(
-  'Todos os produtos do departamento de Informatica disponíveis no estoque...: ',
-);
+console.log('Todos os produtos do departamento de Informatica disponíveis no estoque: ');
 
 departamentoInformatica.exibirProdutos(
   'Mouse',
