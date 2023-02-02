a6f3a62b7f590af16212faf361ea27b9f67df0b8
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2023-02-02 19:00:29 -0300

    Aula 39

--- modulo-03/aula-39-loop-for/loop-for.ts
+++ modulo-03/aula-39-loop-for/loop-for.ts
@@ -1,11 +1,3 @@
-/* eslint-disable prettier/prettier */
-/**
- * arquivo: loop-for.ts
- * descrição: arquivo responsável por ensinar como usar o loop for no Typescript
- * data: 23/01/2021
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- */
-
 // ==> Exemplo 01: loop for
 
 for (let i = 0; i < 10; i++) {
