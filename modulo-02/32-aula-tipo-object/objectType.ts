f376dc066ae6301a470bac29e3f223728a32216f
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2023-02-02 14:38:42 -0300

    aula 32

--- modulo-02/32-aula-tipo-object/objectType.ts
+++ modulo-02/32-aula-tipo-object/objectType.ts
@@ -1,11 +1,3 @@
-/* eslint-disable prettier/prettier */
-/**
- * arquivo: objectType.ts
- * descrição: arquivo responsável por ensinar conceitos básicos sobre o tipo 'object'
- * data: 26/11/2021
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- * doc referência: https://www.typescriptlang.org/docs/handbook/2/objects.html
- */
 export { };
 
 // ==> Exemplo 01 -> Exemplo básico do uso do Type Object
@@ -15,16 +7,17 @@ const pessoa = {
   sobrenome: 'Lemos',
   idade: 35,
   funcao: 'Cloud Advocate'
-};
+}
 
 console.log(pessoa);
 
 // ==> Exemplo 02 -> object como parâmetros de função (eles podem ser anônimos)
-function onboarding01(funcionario: { nome: string }) {
-  return 'Seja bem-vinda ' + funcionario.nome;
+
+function onboarding01(funcionario: { nome: string}) {
+  return `Seja bem-vinda ${funcionario.nome}`;
 }
 
-console.log(onboarding01({ nome: 'Glaucia Lemos' }));
+console.log(onboarding01( { nome: 'Glaucia Lemos' } ));
 
 // ==> Exemplo 03 -> object nomeados
 
@@ -34,15 +27,10 @@ interface Pessoa {
 }
 
 function onboarding02(pessoa: Pessoa) {
-  return (
-    'Seja bem-vinda ' +
-    pessoa.nome +
-    '!' +
-    ' Sua função aqui na empresa será ' +
-    pessoa.funcao +
-    '.'
+  return(
+    `Seja bem-vinda ${pessoa.nome}! Sua função aqui na empresa será ${pessoa.funcao}`
   );
-}
+};
 
 console.log(onboarding02({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate' }));
 
@@ -55,21 +43,12 @@ type Pessoa03 = {
 }
 
 function onboarding03(pessoa: Pessoa03) {
-  return (
-    'Seja bem-vinda ' +
-    pessoa.nome +
-    '!' +
-    ' Sua função aqui na empresa será ' +
-    pessoa.funcao +
-    '.' +
-    ' Você trabalhará com a linguagem ' +
-    pessoa.linguagem +
-    '.'
+  return(
+    `Seja bem-vinda ${pessoa.nome}! Sua função aqui na empresa será ${pessoa.funcao}. Você trabalhará com a linguagem ${pessoa.linguagem}.`
   );
-}
+};
 
-console.log(onboarding03({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate', linguagem: 'JavaScript/Typescript' }));
-
+console.log(onboarding03({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate', linguagem: 'JavaScript/TypeScript'}));
 
 // ==> Exemplo 05 -> usando optional no object
 
@@ -77,24 +56,16 @@ interface Pessoa04 {
   nome: string;
   funcao: string;
   linguagem: string;
-  email?: string;
+  email?: string; // Dado Opcional
 }
 
 function onboarding04(pessoa: Pessoa04) {
-  return (
-    'Seja bem-vinda ' +
-    pessoa.nome +
-    '!' +
-    ' Sua função aqui na empresa será ' +
-    pessoa.funcao +
-    '.' +
-    ' Você trabalhará com a linguagem ' +
-    pessoa.linguagem +
-    '.'
+  return(
+    `Seja bem-vinda ${pessoa.nome}! Sua função aqui na empresa será ${pessoa.funcao}. Você trabalhará com a linguagem ${pessoa.linguagem}.`
   );
-}
+};
 
-console.log(onboarding04({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate', linguagem: 'JavaScript/Typescript' }));
+console.log(onboarding04({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate', linguagem: 'JavaScript/TypeScript'}));
 
 // ==> Exemplo 06 -> Propriedade 'readonly' (se deseja proibir que os devs não modifiquem um determinado objeto use o '
 //readonly)
@@ -106,32 +77,21 @@ interface Pessoa05 {
   readonly email: string;
 }
 
-function onboarding05(pessoa: Pessoa05) {
-  return (
-    'Seja bem-vinda ' +
-    pessoa.nome +
-    '!' +
-    ' Sua função aqui na empresa será ' +
-    pessoa.funcao +
-    '.' +
-    ' Você trabalhará com a linguagem ' +
-    pessoa.linguagem +
-    '.' +
-    ' Seu e-mail será ' +
-    pessoa.email
+function onboarding05(pessoa: Pessoa04) {
+  return(
+    `Seja bem-vinda ${pessoa.nome}! Sua função aqui na empresa será ${pessoa.funcao}. Você trabalhará com a linguagem ${pessoa.linguagem}. Seu e-mail será ${pessoa.email}`
   );
-}
+};
 
-console.log(onboarding05(
-  {
-    nome: 'Glaucia Lemos',
-    funcao: 'Cloud Advocate',
-    linguagem: 'JavaScript/Typescript',
-    email: 'gllemos@microsoft.com'
-  }
-));
+console.log(onboarding05({
+  nome: 'Glaucia Lemos',
+  funcao: 'Cloud Advocate',
+  linguagem: 'JavaScript/TypeScript',
+  email: 'gllemos@microsoft.com'
+}));
 
 // ==> Exemplo 07 -> tipos de extensões (heranças)
+
 interface Mae {
   nome: string;
 }
@@ -185,7 +145,7 @@ const usuario: Usuario = {
 const admin: Admin = {
   nome: 'Glaucia Lemos',
   email: 'algumacoisa@gmail.com',
-  admin: true
+  admin: true,
 }
 
 function acessarSistema<T>(usuario: T): T {
@@ -197,6 +157,6 @@ console.log(acessarSistema<Admin>(admin));
 
 /*function acessarSistema(usuario: Usuario): Usuario {
   return usuario;
-};*/
+}
 
-// console.log(acessarSistema(usuario));
+console.log(acessarSistema(usuario));*/
