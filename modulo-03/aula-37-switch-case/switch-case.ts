064962ffe87d61c8d0cdce70462b9f86916c4af5
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2023-02-02 19:00:21 -0300

    Aula 37

--- modulo-03/aula-37-switch-case/switch-case.ts
+++ modulo-03/aula-37-switch-case/switch-case.ts
@@ -1,12 +1,3 @@
-/* eslint-disable prefer-const */
-/* eslint-disable prettier/prettier */
-/**
- * arquivo: switch-case.ts
- * descrição: arquivo responsável por ensinar como usar instrução switch-case no Typescript
- * data: 23/12/2021
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- */
-
 export { };
 
 // ==> Exemplo 01 - Uso do switch...case
