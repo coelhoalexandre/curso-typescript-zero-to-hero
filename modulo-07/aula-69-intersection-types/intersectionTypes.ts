038030a5966a8a9c5f5d73a50065ab8025bf85ed
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2023-02-04 08:24:16 -0300

    aula 69

--- modulo-07/aula-69-intersection-types/intersectionTypes.ts
+++ modulo-07/aula-69-intersection-types/intersectionTypes.ts
@@ -2,7 +2,31 @@ export = {};
 
 // ==> Exemplo 01 - Intersection Types:
 
+interface DadosBancarios {
+  conta: number;
+  agencia: number;
+  banco: string;
+}
 
+interface Cliente {
+  nome: string;
+  email: string;
+}
 
-// ==> Exemplo 02 - Intersection Types:
+interface DadosPessoaFisica {
+  cpf: number;
+}
+
+type DadosCliente = DadosBancarios & Cliente & DadosPessoaFisica;
+
+const dadosCliente: DadosCliente = {
+  conta: 12346,
+  agencia: 123,
+  banco: 'Banco do Brasil',
+  nome: 'Glaucia Lemos',
+  email: 'glauica@email.com',
+  cpf: 123456789,
+}
+
+console.log(dadosCliente);
 
